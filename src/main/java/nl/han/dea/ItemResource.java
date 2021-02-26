0eaf569a8a8f64cf93ca84fb883d8e75381b0152
Stef Schöningh <stef.schoningh1@gmail.com>
2021-02-26 11:11:55 +0100

    initial commit (starting from ex. 7)

---
+++ src/main/java/nl/han/dea/ItemResource.java
@@ -0,0 +1,39 @@
+package nl.han.dea;
+
+import nl.han.dea.services.ItemService;
+import nl.han.dea.services.dto.ItemDTO;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/items")
+public class ItemResource {
+
+    private ItemService itemService;
+
+    public ItemResource() {
+        this.itemService = new ItemService();
+    }
+
+    @GET
+    @Path("/")
+    @Produces("text/plain")
+    public String itemsAsText() {
+        return "bread, butter";
+    }
+
+    @GET
+    @Path("/")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response itemsAsJson() {
+//         je kan ook zo een ok status met de response sturen:
+//        return Response
+//            .status(Response.Status.OK)
+//            .entity(itemService.getAll())
+//            .build();
+        return Response.ok(itemService.getAll()).build();
+    }
+}
